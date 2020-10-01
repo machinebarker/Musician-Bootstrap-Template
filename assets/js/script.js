46702ac09da4d2d9f8c6fbb3c3c0b9af771f553f
Machine Barker <machinebarkers@gmail.com>
2020-10-01 08:15:34 +0700

    modify the template and add a new page

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,29 @@ function writeText() {
     }
 }
 setInterval(writeText, 150);
+
+const toggler = document.querySelector('.navbar-toggler');
+var navbar = document.querySelector('.navbar');
+var prevScroll = window.pageYOffset;
+
+window.onscroll = function () {
+    var currentScroll = window.pageYOffset;
+    if (prevScroll > currentScroll) {
+        navbar.style.top = "0";
+        navbar.style.background = "rgba(0, 0, 0, .8)";
+    } else {
+        navbar.style.top = "-50%";
+        navbar.style.transition = ".5s";
+        navbar.style.background = "transparent";
+    }
+    prevScroll = currentScroll;
+}
+
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preload');
+    preloader.classList.add('preload-finish')
+});
+
+// toggler.addEventListener('click', () => {
+//     navbar.classList.toggle('nav-bg');
+// });
